9bd134322bc018f13f43f14f54f0794dd65bc642
mao-at <maohan.sun@antra.com>
2023-06-21 12:16:44 -0400

    added unit testing

---
+++ python/pytest_demo/test_calculator.py
@@ -0,0 +1,27 @@
+import pytest
+from calculator import Calculator
+
+def test_add():
+    calculator = Calculator()
+    result = calculator.add(3, 4)
+    assert result == 7
+
+def test_subtract():
+    calculator = Calculator()
+    result = calculator.subtract(10, 5)
+    assert result == 5
+
+def test_multiply():
+    calculator = Calculator()
+    result = calculator.multiply(4, 5)
+    assert result == 20
+
+def test_divide():
+    calculator = Calculator()
+    result = calculator.divide(10, 2)
+    assert result == 6
+
+def test_divide_by_zero():
+    calculator = Calculator()
+    with pytest.raises(ValueError):
+        calculator.divide(10, 0)
